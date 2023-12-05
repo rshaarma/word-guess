b6590ca5e606ea276fa332ecab85c2825f9029f9
rshaarma <rohittsharrma@outlook.com>
2023-12-05 23:38:31 +0000

    add questions in js file

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,72 @@
+var questions = [
+  {
+    title:
+      "The city of Venice is flooded every four years. The locals call this high water or acqua alta. What would you do during acqua alta?",
+    choices: [
+      "Jump in puddles and sing in the rain. I love water! ",
+      "Look for a dry place and hang tight until the water recedes ",
+      "Stroll the piazza without a care in the world. The weather can't stop me. ",
+      "Leave ASAP ",
+    ],
+    answer:
+      "Stroll the piazza without a care in the world. The weather can't stop me. ",
+  },
+  {
+    title: "Which movie would you watch on a sleepy Sunday afternoon?",
+    choices: [
+      "Titanic ",
+      "20,000 Leagues Under the Sea ",
+      "Lawrence of Arabia ",
+      "The Endless Summer ",
+    ],
+    answer: "The Endless Summer ",
+  },
+  {
+    title:
+      "One in three youngsters aged 9 to 11 were unable to name this British beauty – but can you?",
+    choices: ["Magpie", "Pied wagtail", "Jay", "Great woodpecker"],
+    answer: "Magpie",
+  },
+  {
+    title:
+      "A friend invites you to the Great California Hippie Commune to grow your own crops and have a nice chat with local spirits. How do you respond?",
+    choices: [
+      "'Weirdo alert. He's out of his mind, I'm not going anywhere ",
+      "I might visit, but I'd have to make sure it's not dangerous ",
+      "Ancient spirits, here I come! ",
+      "'d be glad to spend some time and explore the area ",
+    ],
+    answer: "'d be glad to spend some time and explore the area ",
+  },
+  {
+    title: "Who was the Ancient Greek God of the Sun? ",
+    choices: ["Apollo", "Apollo2", "Sigma43", "Lunar 1"],
+    answer: "Apollo",
+  },
+  {
+    title: "What year was the United Nations established?",
+    choices: ["1945", "1888", "1984", "2000"],
+    answer: "1945",
+  },
+  {
+    title: "How many minutes are in a full week? ",
+    choices: ["27464", "10089", "3648563", "12351"],
+    answer: "10,080",
+  },
+  {
+    title: "Aureolin is a shade of what color? ",
+    choices: ["Yellow", "Green", "Blue", "Red"],
+    answer: "Yellow",
+  },
+  {
+    title: "How many ghosts chase Pac-Man at the start of each game?",
+    choices: ["6", "9", "13", "4"],
+    answer: "4",
+  },
+
+  {
+    title: "Which planet in the Milky Way is the hottest?",
+    choices: ["Venus", "Moon", "Venus II", "All the above"],
+    answer: "Venus",
+  },
+];
