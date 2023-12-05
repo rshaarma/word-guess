baba17be3a57734efc8ee3a87c271d9328ccd849
rshaarma <rohittsharrma@outlook.com>
2023-12-05 20:09:47 +0000

    added assets and starter folders

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,12 @@
+var questions = [
+  {
+    question: "How old are you?",
+    wrongChoices: ["20", "40", "67"],
+    correctChoices: ["55"],
+  },
+  {
+    question: "How many pets you have?",
+    wrongChoices: ["12", "4", "6"],
+    correctChoices: ["5"],
+  },
+];
